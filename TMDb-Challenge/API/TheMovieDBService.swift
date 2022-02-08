d3ca394bccaa8fe9fe7a4e60be5d4f447610fe13
johncoimbra <johnallen@outlook.com.br>
2022-02-08 15:05:59 -0300

    Fixed API Service

--- TMDb-Challenge/API/TheMovieDBService.swift
+++ TMDb-Challenge/API/TheMovieDBService.swift
@@ -100,47 +100,47 @@ class TheMovieDBService {
         }
     }
     
-    // MARK: - Get Details
-    func fetchPopular(movie_id: Int, onComplete: @escaping (MovieDetails?) -> Void) {
-        let url: String
-        url = "\(API_BASE)\(MOVIE)\(movie_id)?api_key=\(API_KEY)"
-        print("DEBUG: url Details...: \(url)")
-        
-        Alamofire.request(url).responseJSON { (response) in
-            guard let data = response.data else {
-                onComplete(nil)
-                return
-            }
-            do {
-                let movieDetailInfo = try JSONDecoder().decode(MovieDetails.self, from: data)
-                onComplete(movieDetailInfo)
-            } catch {
-                print(error.localizedDescription)
-                onComplete(nil)
-            }
-        }
-    }
-    
-    // MARK: - Get Similar Movies
-    func fetchPopular(movie_id: Int, page: Int, onComplete: @escaping (Similar?) -> Void) {
-        let url: String
-        url = "\(API_BASE)\(MOVIE)\(movie_id)\(SIMILAR)api_key=\(API_KEY)&page=\(page)"
-        print("DEBUG: url Similar...: \(url)")
-        
-        Alamofire.request(url).responseJSON { (response) in
-            guard let data = response.data else {
-                onComplete(nil)
-                return
-            }
-            do {
-                let similarInfo = try JSONDecoder().decode(Similar.self, from: data)
-                onComplete(similarInfo)
-            } catch {
-                print(error.localizedDescription)
-                onComplete(nil)
+    //MARK: - Get Details
+        func fetchMovieDetails(movie_id: Int, onComplete: @escaping (MovieDetails?) -> Void) {
+            let url: String
+            url = "\(API_BASE)\(MOVIE)\(movie_id)?api_key=\(API_KEY)"
+            print("DEBUG: url...:\(url)")
+            
+            Alamofire.request(url).responseJSON { (response) in
+                guard let data = response.data else {
+                    onComplete(nil)
+                    return
+                }
+                do {
+                    let nowPlayingInfo = try JSONDecoder().decode(MovieDetails.self, from: data)
+                    onComplete(nowPlayingInfo)
+                } catch {
+                    print(error.localizedDescription)
+                    onComplete(nil)
+                }
             }
         }
-    }
+        
+        //MARK: - Get Similar Movies
+        func fetchSimilarMovies(movie_id: Int, page: Int, onComplete: @escaping (Similar?) -> Void) {
+            let url: String
+            url = "\(API_BASE)\(MOVIE)\(movie_id)\(SIMILAR)api_key=\(API_KEY)&page=\(page)"
+            print("DEBUG: url Similar...:\(url)")
+            
+            Alamofire.request(url).responseJSON { (response) in
+                guard let data = response.data else {
+                    onComplete(nil)
+                    return
+                }
+                do {
+                    let nowPlayingInfo = try JSONDecoder().decode(Similar.self, from: data)
+                    onComplete(nowPlayingInfo)
+                } catch {
+                    print(error.localizedDescription)
+                    onComplete(nil)
+                }
+            }
+        }
     
     // MARK: - Get Genre
     func fetchPopular(movie_id: Int, onComplete: @escaping (GenreResponse?) -> Void) {
